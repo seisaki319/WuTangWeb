6aa7a8dc0dc6311f7d38c090f81eff3eb62603b4
Seiji Sakiyama <seisaki319@berkeley.edu>
2017-08-09 02:54:32 -0700

    Allowedhost

--- WuTangWeb/settings.py
+++ WuTangWeb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'xxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*.pythonanywhere.com",]
+ALLOWED_HOSTS = [".pythonanywhere.com",]
 
 
 # Application definition
